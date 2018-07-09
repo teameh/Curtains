a344f47d6d1b5e30a25713466bf5cfdf92f82602
Tieme van Veen <tieme.van.veen@persgroep.net>
2018-07-09 18:39:28 +0200

    wip on iOS App

--- CurtainsiOS/CurtainsiOS/ViewController.swift
+++ CurtainsiOS/CurtainsiOS/ViewController.swift
@@ -15,33 +15,71 @@ class ViewController: UIViewController {
     @IBOutlet weak var saveButton: UIButton!
     @IBOutlet weak var statusLog: UITextView!
     
+    @IBOutlet weak var stepsSlider: UISlider!
+    @IBOutlet weak var speedSlider: UISlider!
+    @IBOutlet weak var accelSlider: UISlider!
+    @IBOutlet weak var maxSlider: UISlider!
+
+    @IBOutlet weak var stepsLabel: UILabel!
+    @IBOutlet weak var speedLabel: UILabel!
+    @IBOutlet weak var accelLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         statusLog.text = "";
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.onChangeSteps(self)
+        self.onChangeSpeed(self)
+        self.onChangeAccel(self)
+        self.onMaxSliderChange(self)
+    }
+    
+    @IBAction func onChangeSteps(_ sender: Any) {
+        stepsLabel.text = String(format: "%.0f", stepsSlider.value)
+    }
+    
+    @IBAction func onChangeSpeed(_ sender: Any) {
+        speedLabel.text = String(format: "%.0f", speedSlider.value)
+    }
+    
+    @IBAction func onChangeAccel(_ sender: Any) {
+        accelLabel.text = String(format: "%.0f", accelSlider.value)
+    }
+    
+    @IBAction func onMaxSliderChange(_ sender: Any) {
+        speedSlider.maximumValue = maxSlider.value
+        accelSlider.maximumValue = maxSlider.value
+        
+        self.onChangeSpeed(self)
+        self.onChangeAccel(self)
     }
 
     @IBAction func onSave(_ sender: UIButton) {
         var intervalSinceNow: Int = Int(datePicker.date.timeIntervalSinceNow);
-        self.statusLog.text = "Saving... " + "\n" + self.statusLog.text;
+        self.statusLog.text = "Saving... \n\n";
         
         if(intervalSinceNow <= 0) {
-            self.statusLog.text = "intervalSinceNow <= 0... set to 5" + "\n" + self.statusLog.text;
+            self.statusLog.text = self.statusLog.text + "intervalSinceNow <= 0... rotate in 5 seconds..\n\n";
             intervalSinceNow = 5;
         }
         
-        Alamofire.request("http://192.168.2.22/steps/40000/4000/2500/\(intervalSinceNow)/").responseJSON { response in
-            print("Request: \(String(describing: response.request))")   // original url request
-            print("Response: \(String(describing: response.response))") // http url response
-            print("Result: \(response.result)")                         // response serialization result
-            
-            self.statusLog.text = "Response: \(String(describing: response.response))" + "\n" + self.statusLog.text;
-            
-            if let json = response.result.value {
-                print("JSON: \(json)") // serialized json response
-                self.statusLog.text = "\(json)" + "\n" + self.statusLog.text;
+        let steps = String(format: "%.0f", stepsSlider.value);
+        let speed = String(format: "%.0f", speedSlider.value);
+        let accel = String(format: "%.0f", accelSlider.value);
+        
+        Alamofire.request("http://192.168.2.22/steps/\(steps)/\(speed)/\(accel)/\(intervalSinceNow)/")
+            .validate()
+            .responseJSON { response in
+                switch response.result {
+                case .success:
+                    self.statusLog.text = self.statusLog.text + "Succes!\n\n";
+                    if let json = response.result.value {
+                        self.statusLog.text = self.statusLog.text + "Response JSON: \(json) \n\n";
+                    }
+                case .failure(let error):
+                    self.statusLog.text = self.statusLog.text + "Error.. \(error) \n\n";
+                }
             }
-        }
     }
 }
 
