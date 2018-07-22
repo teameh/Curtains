35b82b6ce5d478ac699e74205c99c482fd1c7152
Tieme van Veen <tiemevanveen@hotmail.com>
2018-07-22 16:51:52 +0200

    time server fixes

--- src/main.cpp
+++ src/main.cpp
@@ -52,7 +52,7 @@ byte packetBuffer[NTP_PACKET_SIZE];
 WiFiUDP udp;
 WiFiUDP Udp;
 
-int secondsGtmOffset = 3600; // GTM+1
+int secondsGtmOffset = 7200; // GTM+1
 int prevMinute = 0;
 int prevSecond = 0;
 int timeSyncFailCount = 0;
@@ -435,7 +435,16 @@ void sendNTPpacket(IPAddress& address)
 time_t getNtpTime()
 {
   Serial.println("- getNtpTime");
-  WiFi.hostByName(ntpServerName, timeServerIP); 
+  
+  if(!timeServerIP) {
+    Serial.print("get timeServerIP by hostname lookup...: ");
+    WiFi.hostByName(ntpServerName, timeServerIP);
+    Serial.println(timeServerIP);
+  } else {
+    Serial.print("reuse timeServerIP: ");
+    Serial.println(timeServerIP);
+  }
+
   sendNTPpacket(timeServerIP);
   delay(500);
   int cb = udp.parsePacket();
@@ -458,65 +467,6 @@ time_t getNtpTime()
   Serial.println("No NTP Response :-(");
   return 0; // return 0 if unable to get the time
 }
-
-// // send an NTP request to the time server at the given address
-// void sendNTPpacket(IPAddress &address)
-// {
-//   Serial.println("- sendNTPpacket");
-//   // set all bytes in the buffer to 0
-//   memset(packetBuffer, 0, NTP_PACKET_SIZE);
-//   // Initialize values needed to form NTP request
-//   // (see URL above for details on the packets)
-//   packetBuffer[0] = 0b11100011;   // LI, Version, Mode
-//   packetBuffer[1] = 0;     // Stratum, or type of clock
-//   packetBuffer[2] = 6;     // Polling Interval
-//   packetBuffer[3] = 0xEC;  // Peer Clock Precision
-//   // 8 bytes of zero for Root Delay & Root Dispersion
-//   packetBuffer[12]  = 49;
-//   packetBuffer[13]  = 0x4E;
-//   packetBuffer[14]  = 49;
-//   packetBuffer[15]  = 52;
-//   // all NTP fields have been given values, now
-//   // you can send a packet requesting a timestamp:                 
-//   Udp.beginPacket(address, 123); //NTP requests are to port 123
-//   Udp.write(packetBuffer, NTP_PACKET_SIZE);
-//   Udp.endPacket();
-// }
-
-
-// time_t getNtpTime()
-// {
-//   Serial.println("- getNtpTime");
-  
-//   if(!timeServerIP) {
-//     Serial.print("get timeServerIP by hostname lookup...: ");
-//     WiFi.hostByName(ntpServerName, timeServerIP);
-//     Serial.println(timeServerIP);
-//   } else {
-//     Serial.print("reuse timeServerIP: ");
-//     Serial.println(timeServerIP);
-//   }
-
-//   while (Udp.parsePacket() > 0) ; // discard any previously received packets
-//   sendNTPpacket(timeServerIP);
-//   uint32_t beginWait = millis();
-//   while (millis() - beginWait < 1500) {
-//     int size = Udp.parsePacket();
-//     if (size >= NTP_PACKET_SIZE) {
-//       Serial.println("Receive NTP Response");
-//       Udp.read(packetBuffer, NTP_PACKET_SIZE);  // read packet into the buffer
-//       unsigned long secsSince1900;
-//       // convert four bytes starting at location 40 to a long integer
-//       secsSince1900 =  (unsigned long)packetBuffer[40] << 24;
-//       secsSince1900 |= (unsigned long)packetBuffer[41] << 16;
-//       secsSince1900 |= (unsigned long)packetBuffer[42] << 8;
-//       secsSince1900 |= (unsigned long)packetBuffer[43];
-//       return secsSince1900 - 2208988800UL + secondsGtmOffset;
-//     }
-//   }
-//   Serial.println("No NTP Response :-(");
-//   return 0; // return 0 if unable to get the time
-// }
 
 void printDigits(int digits){
   // utility for digital clock display: prints preceding colon and leading 0
@@ -541,7 +491,7 @@ void printDateTime(){
 
 void decreaseTimeSyncInterval() {
   Serial.println("- decreaseTimeSyncInterval");
-  timeSyncInterval = 5 * 60;
+  timeSyncInterval = 15 * 60;
   setSyncInterval(timeSyncInterval);
   // indicate time is synced
   digitalWrite(0, LOW);
@@ -551,8 +501,9 @@ void decreaseTimeSyncInterval() {
 void onceEverySecond() {
   printDateTime();
 
+  bool timeIsSynced = now() > 1514764800; // 2018
   // Decrease sync interval if time is synced or we've tried too many times
-  if(timeSyncInterval == 10 && (now() > 1514764800 || timeSyncFailCount > 10)) {
+  if(timeSyncInterval == 10 && (timeIsSynced || timeSyncFailCount > 10)) {
     decreaseTimeSyncInterval();
   }
 }
